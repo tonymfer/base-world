38a33c753bd581fc0708d77166026028c16f02d7
Brandon Garvajal <brandon.garvajal@gmail.com>
2024-07-26 22:43:44 +0700

    chore(leaderboard): add back env wallet connect to env.mjs

--- env.mjs
+++ env.mjs
@@ -5,10 +5,13 @@ export const env = createEnv({
   server: {},
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: z.string().min(1),
     NEXT_PUBLIC_ZORA_URL: z.string().min(1),
   },
   runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID:
+      process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
     NEXT_PUBLIC_ZORA_URL: process.env.NEXT_PUBLIC_ZORA_URL,
   },
 });
